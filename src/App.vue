0e3f49f353b476b746160ba2269af3373a7c9f7d
HanimRosli <shahanim.rosli@gmail.com>
2020-11-16 23:49:58 +0800

    update manage user

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
 <div id="app">
+<<<<<<< Updated upstream
     <div class="container">
         <router-view />
+=======
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="primary">
+            <b-navbar-brand href="#">Dr Ko Self Appointment</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+
+                    <b-navbar-nav>
+                        <b-nav-item href="/register">Appointment</b-nav-item>
+                    </b-navbar-nav>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+>>>>>>> Stashed changes
     </div>
 </div>
 </template>
--- src/views/adminHomepage.vue
+++ src/views/adminHomepage.vue
@@ -6,11 +6,45 @@
         </b-button>
     </div>
 
+<<<<<<< Updated upstream
     <div>
         <b-button variant="light" size="sm">
             <router-link to="/">Manage User</router-link>
         </b-button>
     </div>
+=======
+    <b-card overlay img-src="https://picsum.photos/900/200/?image=3" img-alt="Card Image" text-variant="white" title="Dr Ko Skin Specialist" sub-title="">
+
+    </b-card>
+    <br>
+    <br>
+    <div class="row justify-content-between align-items-center">
+        <div class="col-12 p5">
+            <b-card-group deck>
+                <b-card title="Manage Booking" class="mb-2">
+                    <b-card-text>
+                        Manage booking appointment
+                    </b-card-text>
+
+                    <b-button href="/booking" variant="primary">Manage Booking</b-button>
+                </b-card>
+
+                <b-card title="Manage user" class="mb-2">
+                    <b-card-text>
+                        Manage our clients
+                    </b-card-text>
+
+                    <b-button href="http://localhost:8080/" variant="primary">Manage User</b-button>
+                </b-card>
+
+                <b-card title="Manage staff" class="mb-2">
+                    <b-card-text>
+                        Managing dr ko's staff
+                    </b-card-text>
+
+                    <b-button href="/staff" variant="primary">Manage Staff</b-button>
+                </b-card>
+>>>>>>> Stashed changes
 
     <div>
         <b-button variant="light" size="sm">
